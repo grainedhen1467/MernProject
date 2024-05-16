0bc17a9ca4ca49805609efbb693e1e5d5219cd7d
Jameel Melson <jameel.melson@gmail.com>
2024-05-16 14:38:17 -0700

    Dave Gray mern stack tutorial lesson 2 complete

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,24 @@
-const express = require('express')
-const app = express()
-const path = require('path')
-const PORT = process.env.PORT || 3500
+const express = require('express');
+const app = express();
+const path = require('path');
+const { logger } = require("./middleware/logger");
+const errorHandler = require("./middleware/errorHandler");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const corsOptions = require("./config/corsOptions");
+const PORT = process.env.PORT || 3500;
 
-app.use('/', express.static(path.join(__dirname, '/public')))
+app.use(logger);
 
-app.use('/', require('./routes/root'))
+app.use(cors(corsOptions));
+
+app.use(express.json());
+
+app.use(cookieParser());
+
+app.use('/', express.static(path.join(__dirname, 'public')));
+
+app.use('/', require('./routes/root'));
 
 app.all('*', (req, res) => {
     res.status(404)
@@ -16,6 +29,8 @@ app.all('*', (req, res) => {
     } else {
         res.type('txt').send('404 Not Found')
     }
-})
+});
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+app.use(errorHandler);
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
